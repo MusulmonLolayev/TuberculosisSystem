97b5625e9e6bf0d8b65ff507b5a15e8ff18a2587
MusulmonLolayev <musulmon.lolayev.94@gmail.com>
2020-08-14 17:06:31 +0500

    The bugs with connected selection in v-primary was repaired.

--- frontend/gui-vue/src/components/create/v-primary-diagnose.vue
+++ frontend/gui-vue/src/components/create/v-primary-diagnose.vue
@@ -7,7 +7,9 @@
           :items="CLINICAL_FORMS"
           item-text="name"
           item-value="id"
-          v-model="primarydiagnose.clinicalform"
+          return-object
+          v-model="selectedClinicalform"
+          @change="clinicalChanged"
         />
       </v-col>
       <v-col cols="10" md="3">
@@ -16,7 +18,9 @@
           :items="LOCALIZATIONS"
           item-text="name"
           item-value="id"
-          v-model="primarydiagnose.localization"
+          v-model="selectedLocalization"
+          @change="localizationChanged"
+          return-object
         />
       </v-col>
       <v-col cols="10" md="3">
@@ -25,7 +29,9 @@
           :items="PREVALENCES"
           item-text="name"
           item-value="id"
-          v-model="primarydiagnose.prevalence"
+          v-model="selectedPrevalence"
+          @change="prevalenceChanged"
+          return-object
         />
       </v-col>
       <v-col cols="10" md="3">
@@ -78,22 +84,42 @@ import Helper from "../commons/functions.js";
 export default {
   name: "v-primary-diagnose",
   props: ["primarydiagnose"],
+  data: function(){
+    return{
+      selectedClinicalform: '',
+      selectedLocalization: '',
+      selectedPrevalence: '',
+    }
+  },
   methods: {
     async initialize() {
-      Promise.all([
-        this.GET_CLINICAL_FORMS_FROM_API(),
-        this.GET_LOCALIZATION_FROM_API(),
-        this.GET_PREVALENCES_FROM_API()
-      ]);
+      await this.GET_CLINICAL_FORMS_FROM_API()
+      await this.GET_LOCALIZATION_FROM_API()
+      await this.GET_PREVALENCES_FROM_API()
+
       if (typeof this.primarydiagnose.id == 'undefined'){
-        
         this.primarydiagnose.date =  Helper.GetCurrentDate()
 
-        /*// then default vaules to primary diagnose
-        this.primarydiagnose.clinicalform = this.CLINICAL_FORMS[0].id
-        this.primarydiagnose.localization = this.LOCALIZATIONS[0].id
-        this.primarydiagnose.prevalence = this.PREVALENCES[0].id*/
+        // then default vaules to primary diagnose
+        this.selectedClinicalform = this.CLINICAL_FORMS[0].id
+        this.selectedLocalization = this.LOCALIZATIONS[0].id
+        this.selectedPrevalence = this.PREVALENCES[0].id
       }
+      else{
+        // then default vaules to primary diagnose
+        this.selectedClinicalform = this.primarydiagnose.clinicalform
+        this.selectedLocalization = this.primarydiagnose.localization
+        this.selectedPrevalence = this.primarydiagnose.prevalence
+      }
+    },
+    clinicalChanged(id){
+      this.primarydiagnose.clinicalform = id
+    },
+    localizationChanged(id){
+      this.primarydiagnose.localization = id
+    },
+    prevalenceChanged(id){
+      this.primarydiagnose.prevalence = id
     },
     ...mapActions([
       "GET_CLINICAL_FORMS_FROM_API",
