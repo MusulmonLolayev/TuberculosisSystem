20b97b95d2e4a86154cfa6b62c810ce79e5f5d75
MusulmonLolayev <musulmon.lolayev.94@gmail.com>
2020-08-16 12:32:58 +0500

    The deleteInstance function in function was created.

--- frontend/gui-vue/src/components/commons/functions.js
+++ frontend/gui-vue/src/components/commons/functions.js
@@ -100,4 +100,18 @@ export default {
         return e
       }
     },
+
+    async deleteInstance(instance, url){  
+      try{
+        if (confirm('Do you want to delete')){
+          await Api.delete(url, {
+            data: { instance }
+          })
+          return true
+        }
+      }
+      catch (e){
+        return e
+      }
+    },
 }
