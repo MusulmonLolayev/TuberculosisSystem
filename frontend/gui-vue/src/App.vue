bc19145c030f9db001ea4f8561f02611bd269711
MusulmonLolayev <musulmon.lolayev.94@gmail.com>
2020-08-17 19:00:55 +0500

    The alert and message box was added into app.vue

--- frontend/gui-vue/src/App.vue
+++ frontend/gui-vue/src/App.vue
@@ -59,13 +59,20 @@
     >
     <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <router-link to="/" class="router-link">Home</router-link>
-      <router-link to="/news" class="router-link">News</router-link>
+      <!--<router-link to="/news" class="router-link">News</router-link>-->
       <router-link to="/about" class="router-link">About</router-link>
     </v-app-bar>
 
     <!-- Sizes your content based upon application components -->
     <v-main>
       <v-container fluid>
+        <!-- Loader -->
+        <v-overlay :value="$store.state.loader" absolute light>
+          <v-progress-circular size="64"></v-progress-circular>
+        </v-overlay>
+        <!-- Alert and message box -->
+        <v-message-box ref="message"/>
+        <v-alert-box ref="alert"/>
         <!-- If using vue-router -->
         <router-view></router-view>
       </v-container>
@@ -79,6 +86,11 @@
 </template>
 
 <script>
+import {Api} from './api/Api'
+import store from './vuex/store.js'
+import vMessageBox from "./components/commons/v-alert-box";
+import vAlertBox from "./components/commons/v-message-box"
+
 
 export default {
   name: 'App',
@@ -105,6 +117,40 @@ export default {
       this.$router.push('/datamining')
     },
   },
+  created() {
+    // Add a request interceptor
+    Api.interceptors.request.use(function (config) {
+        // Do something before request is sent
+        store.commit('LOADER', true)
+        return config;
+    }, function (error) {
+    // Do something with request error
+      store.commit('LOADER', false)
+      return Promise.reject(error);
+    });
+
+    // Add a response interceptor
+    Api.interceptors.response.use(function (response) {
+        // Any status code that lie within the range of 2xx cause this function to trigger
+        // Do something with response data
+        store.commit('LOADER', false)
+        return response;
+      }, function (error) {
+        // Any status codes that falls outside the range of 2xx cause this function to trigger
+        // Do something with response error
+        store.commit('LOADER', false)
+        return Promise.reject(error);
+      });
+  },
+  mounted(){
+    store.state.alert = this.$refs['alert']
+    store.state.message = this.$refs['message']
+  },
+  components: {
+    vMessageBox,
+    vAlertBox,
+  },
+
 };
 </script>
 
