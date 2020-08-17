c8a02f1cc5dc11cfdae3e792b117538eb9380b12
MusulmonLolayev <musulmon.lolayev.94@gmail.com>
2020-08-17 19:02:51 +0500

    The axios and some uneseccary files were removed.

--- frontend/gui-vue/src/main.js
+++ frontend/gui-vue/src/main.js
@@ -5,26 +5,18 @@ import Router from 'vue-router'
 import Vuetify from 'vuetify/lib'
 import vuetify from '@/plugins/vuetify' // path to vuetify export
 import store from './vuex/store'
-import Axios from 'axios'
 
 Vue.use(Vuetify)
 Vue.use(Router)
 
 Vue.config.productionTip = false
 
-Vue.prototype.$http = Axios
-const token = localStorage.getItem('token')
-
-if (token){
-  Vue.prototype.$http.defaults.headers.common['Authorization'] = token
-}
-
 new Vue({
+  store,
   el: '#app',
   router,
   render: h => h(App),
   template: '<App />',
   components: {App},
   vuetify,
-  store
 }).$mount('#app')
