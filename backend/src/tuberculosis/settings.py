c7ddd6e09415c28bbf6a01d65f77efbed35b3100
MusulmonLolayev <musulmon.lolayev.94@gmail.com>
2020-08-20 12:10:02 +0500

    The corsheaders app was removed from the INSTALLED_APPS.

--- backend/src/tuberculosis/settings.py
+++ backend/src/tuberculosis/settings.py
@@ -41,9 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'corsheaders',
     'rest_framework.authtoken',
-#    'bootstrap4'
 ]
 
 MIDDLEWARE = [
