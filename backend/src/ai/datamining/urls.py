bf957842860e143645359bdfe3bf9a8f9cbe75d0
MusulmonLolayev <musulmon.lolayev.94@gmail.com>
2020-08-20 11:05:11 +0500

    The GetRangesUpdatingStatus function was added into ai.common.url.py.

--- backend/src/ai/datamining/urls.py
+++ backend/src/ai/datamining/urls.py
@@ -2,15 +2,15 @@ from django.urls import path
 
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from .views import RangeListView, UpdateAcceptableIntevals_as_View
+from .views import RangeListView, UpdateAcceptableIntevals_as_View, GetRangesUpdatingStatus
 
 urlpatterns = [
     
     # Get requests
     # GetAccetableIntervals
     path('getaccetableintervals', RangeListView.as_view()),
-    #path('getaccetableintervals/<method>', GetAccetableIntervals, name='getaccetableintervals'),
     path('updateaccetableintervals/<method>', UpdateAcceptableIntevals_as_View, name='updateaccetableintervals'),
+    path('GetRangesUpdatingStatus', GetRangesUpdatingStatus, name='GetRangesUpdatingStatus'),
 
 ]
 
