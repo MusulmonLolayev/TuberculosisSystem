f1c72741ac5b755af996771bf81f214d541ae00b
MusulmonLolayev <musulmon.lolayev.94@gmail.com>
2020-08-22 20:32:08 +0500

    The permission classes were applied and login function was removed.

--- backend/src/api_app/views.py
+++ backend/src/api_app/views.py
@@ -1,7 +1,10 @@
 from rest_framework.generics import ListAPIView
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
 
 from patientapp.models import *
 
@@ -10,12 +13,13 @@ from .serializer import *
 from ai.settings import GLOBAL_UPDATINGS
 
 class CountryListView(ListAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
 
 class RegionListView(ListAPIView):
     serializer_class = CountrySerializer
-
+    permission_classes = [IsAuthenticated]
     def get_queryset(self):
         """
         This view should return a list of all the regions
@@ -26,7 +30,7 @@ class RegionListView(ListAPIView):
 
 class DistrictListView(ListAPIView):
     serializer_class = DistrictSerializer
-
+    permission_classes = [IsAuthenticated]
     def get_queryset(self):
         """
         This view should return a list of all the districts
@@ -36,28 +40,33 @@ class DistrictListView(ListAPIView):
         return District.objects.filter(region_id=regionId)
 
 class OccupationListView(ListAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Occupation.objects.all()
     serializer_class = OccupationSerializer
 
 class ClinicalFormListView(ListAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = ClinicalForm.objects.all()
     serializer_class = ClinicalFormSerializer
 
 class LocalizationListView(ListAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Localization.objects.all()
     serializer_class = LocalizationSerializer
 
 class PrevalenceListView(ListAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Prevalence.objects.all()
     serializer_class = PrevalenceSerializer
 
 class CharacterOfStoolListView(ListAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = CharacterOfStool.objects.all()
     serializer_class = CharacterOfStoolSerializer
 
 class PrimaryDiagnoseListView(ListAPIView):
     serializer_class = PrimaryDiagnoseSerializer
-
+    permission_classes = [IsAuthenticated]
     def get_queryset(self):
 
         patient_id = self.kwargs['patient_id']
@@ -65,7 +74,7 @@ class PrimaryDiagnoseListView(ListAPIView):
 
 class TakingMedicineListView(ListAPIView):
     serializer_class = TakingMedicineSerializer
-
+    permission_classes = [IsAuthenticated]
     def get_queryset(self):
 
         patient_id = self.kwargs['patient_id']
@@ -73,7 +82,7 @@ class TakingMedicineListView(ListAPIView):
 
 class ComplaintListView(ListAPIView):
     serializer_class = ComplaintSerializer
-
+    permission_classes = [IsAuthenticated]
     def get_queryset(self):
 
         patient_id = self.kwargs['patient_id']
@@ -81,7 +90,7 @@ class ComplaintListView(ListAPIView):
 
 class BloodListView(ListAPIView):
     serializer_class = BloodAnalysisSerializer
-
+    permission_classes = [IsAuthenticated]
     def get_queryset(self):
 
         patient_id = self.kwargs['patient_id']
@@ -89,23 +98,25 @@ class BloodListView(ListAPIView):
 
 class OtherListView(ListAPIView):
     serializer_class = OtherSerializer
-
+    permission_classes = [IsAuthenticated]
     def get_queryset(self):
 
         patient_id = self.kwargs['patient_id']
         return Other.objects.filter(patient_id=patient_id)
 
 class QuestionTitleListView(ListAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = QuestionTitle.objects.all()
     serializer_class = QuestionTitleSerializer
 
 class QuestionListView(ListAPIView):
+    permission_classes = [IsAuthenticated]
     serializer_class = QuestionSerializer
     queryset = Question.objects.all()
 
 class InitialQuestionListView(ListAPIView):
     serializer_class = InitialQuestionSerializer
-
+    permission_classes = [IsAuthenticated]
     def get_queryset(self):
 
         patient_id = self.kwargs['patient_id']
@@ -119,6 +130,7 @@ def GetDistrictById(request, districtId):
     return Response('Page not found', status=status.HTTP_404_NOT_FOUND)
 
 @api_view(['GET', 'POST', 'PUT', 'DELETE'])
+@permission_classes([IsAuthenticated])
 def patient_request(request):
     try:
         ins_name = 'instance'
@@ -196,33 +208,41 @@ def general_request(request, ins_class, ins_ser):
         Response(status=500)
 
 @api_view(['POST', 'DELETE', 'PUT'])
+@permission_classes([IsAuthenticated])
 def primary_request(request):
     return general_request(request, PrimaryDiagnose, PrimaryDiagnoseSerializer)
 
 @api_view(['POST', 'DELETE', 'PUT'])
+@permission_classes([IsAuthenticated])
 def taking_request(request):
     return general_request(request, TakingMedicine, TakingMedicineSerializer)
 
 @api_view(['POST', 'DELETE', 'PUT'])
+@permission_classes([IsAuthenticated])
 def complaint_request(request):
     return general_request(request, Complaint, ComplaintSerializer)
 
 @api_view(['POST', 'DELETE', 'PUT'])
+@permission_classes([IsAuthenticated])
 def blood_request(request):
     return general_request(request, BloodAnalysis, BloodAnalysisSerializer)
 
 @api_view(['POST', 'DELETE', 'PUT'])
+@permission_classes([IsAuthenticated])
 def other_request(request):
     return general_request(request, Other, OtherSerializer)
     
 @api_view(['POST', 'DELETE', 'PUT'])
+@permission_classes([IsAuthenticated])
 def question_request(request):
     return general_request(request, Question, QuestionTitle)
 
 @api_view(['POST', 'PUT', 'DELETE'])
+@permission_classes([IsAuthenticated])
 def question_title_request(request):
     return general_request(request, QuestionTitle, QuestionTitleSerializer)
 
 @api_view(['POST', 'PUT', 'DELETE'])
+@permission_classes([IsAuthenticated])
 def initial_question_request(request):
     return general_request(request, InitialQuestion, InitialQuestionSerializer)
