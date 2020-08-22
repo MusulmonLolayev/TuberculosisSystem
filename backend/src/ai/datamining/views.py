91c69365157bd743a9b7d65d83bbfc1659b8a358
MusulmonLolayev <musulmon.lolayev.94@gmail.com>
2020-08-22 20:28:17 +0500

    The perission classses were applied.

--- backend/src/ai/datamining/views.py
+++ backend/src/ai/datamining/views.py
@@ -1,7 +1,9 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.generics import ListAPIView
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from ai.settings import DISCRIPTION_FEATURES, MODELS_NAMES
 from ai.common.functions import AcceptableInterval, GetFeatures, GetAttributes_Names, UpdateAcceptableIntevals
@@ -9,18 +11,23 @@ from ai.models import Range
 from ai.serializer import RangeSerializer
 from ai.settings import GLOBAL_UPDATINGS
 
+
 class RangeListView(ListAPIView):
     serializer_class = RangeSerializer
+
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         update_number = Range.objects.last().udapte_number
         return Range.objects.filter(udapte_number=update_number)
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def GetRangesUpdatingStatus(request):
     return Response(GLOBAL_UPDATINGS, status=200)
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def UpdateAcceptableIntevals_as_View(request, method):
     try:
         # Check there are any updating.
