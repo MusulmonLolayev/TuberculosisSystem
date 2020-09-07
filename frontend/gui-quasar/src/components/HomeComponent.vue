ba7926281cd1103888dd7b046690b915e337af15
MusulmonLolayev <musulmon.lolayev.94@gmail.com>
2020-09-07 12:17:49 +0500

    The ExampleComponent was changed into HomeComponent.

---
+++ frontend/gui-quasar/src/components/HomeComponent.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <h3>{{$t('app_name')}}</h3>
+  </div>
+</template>
+
+<script lang="ts">
+import {
+  defineComponent, PropType, computed, ref, toRef, Ref,
+} from '@vue/composition-api';
+import { Todo, Meta } from './models';
+
+function useClickCount() {
+  const clickCount = ref(0);
+  function increment() {
+    clickCount.value += 1;
+    return clickCount.value;
+  }
+
+  return { clickCount, increment };
+}
+
+function useDisplayTodo(todos: Ref<Todo[]>) {
+  const todoCount = computed(() => todos.value.length);
+  return { todoCount };
+}
+
+export default defineComponent({
+  name: 'HomeComponent',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    todos: {
+      type: (Array as unknown) as PropType<Todo[]>,
+      default: () => [],
+    },
+    meta: {
+      type: (Object as unknown) as PropType<Meta>,
+      required: true,
+    },
+    active: {
+      type: Boolean,
+    },
+  },
+  setup(props) {
+    return { ...useClickCount(), ...useDisplayTodo(toRef(props, 'todos')) };
+  },
+});
+</script>
