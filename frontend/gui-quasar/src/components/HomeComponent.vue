ee7b91ee7cd0fe6436e28d4ca28cdfaac013bb2e
MusulmonLolayev <musulmon.lolayev.94@gmail.com>
2020-09-07 14:02:46 +0500

    The some example things were removed.

--- frontend/gui-quasar/src/components/HomeComponent.vue
+++ frontend/gui-quasar/src/components/HomeComponent.vue
@@ -6,46 +6,10 @@
 
 <script lang="ts">
 import {
-  defineComponent, PropType, computed, ref, toRef, Ref,
+  defineComponent
 } from '@vue/composition-api';
-import { Todo, Meta } from './models';
-
-function useClickCount() {
-  const clickCount = ref(0);
-  function increment() {
-    clickCount.value += 1;
-    return clickCount.value;
-  }
-
-  return { clickCount, increment };
-}
-
-function useDisplayTodo(todos: Ref<Todo[]>) {
-  const todoCount = computed(() => todos.value.length);
-  return { todoCount };
-}
 
 export default defineComponent({
   name: 'HomeComponent',
-  props: {
-    title: {
-      type: String,
-      required: true,
-    },
-    todos: {
-      type: (Array as unknown) as PropType<Todo[]>,
-      default: () => [],
-    },
-    meta: {
-      type: (Object as unknown) as PropType<Meta>,
-      required: true,
-    },
-    active: {
-      type: Boolean,
-    },
-  },
-  setup(props) {
-    return { ...useClickCount(), ...useDisplayTodo(toRef(props, 'todos')) };
-  },
 });
 </script>
