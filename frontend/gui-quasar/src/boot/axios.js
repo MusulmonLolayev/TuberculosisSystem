f73d025fbe14e19de1e5cd6d25f394afd605cf74
MusulmonLolayev <musulmon.lolayev.94@gmail.com>
2020-09-09 16:11:14 +0500

    The access_token was added into axios when the application runs.

--- frontend/gui-quasar/src/boot/axios.js
+++ frontend/gui-quasar/src/boot/axios.js
@@ -3,14 +3,14 @@ import axios from 'axios'
 const axiosConfig = {
     baseURL: 'http://127.0.0.1:8000/api',
     headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json"
+        Accept: "application/json", "Content-Type": "application/json"
     },
 };
 
 export const Api = axios.create(axiosConfig)
 
 export default ({store, Vue}) => {
+    Api.defaults.headers.common['Authorization'] = `Bearer ${store.state.auth.access_token}`
     Vue.prototype.$axios = Api,
     store.$axios = Api
 }
