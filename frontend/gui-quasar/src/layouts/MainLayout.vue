49bdb65472ec85135526d97e3a4ab28ca48a029e
MusulmonLolayev <musulmon.lolayev.94@gmail.com>
2020-09-09 12:10:27 +0500

    The MainLayout was refactored

--- frontend/gui-quasar/src/layouts/MainLayout.vue
+++ frontend/gui-quasar/src/layouts/MainLayout.vue
@@ -10,98 +10,57 @@
           aria-label="Menu"
           @click="leftDrawerOpen = !leftDrawerOpen"
         />
-
         <q-toolbar-title>
-          Quasar App
+          {{$t('app_name')}}
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <div style="float:right; margin-right:20px" v-if="!$store.state.auth.IsLoggined">
+          <q-btn
+            flat
+            dense
+            borderless
+            :label="$t('login')"
+            to="login"
+          />
+        </div>
+
+        <div style="float:right">
+          <select-localization />
+        </div>
       </q-toolbar>
     </q-header>
-
     <q-drawer
       v-model="leftDrawerOpen"
       show-if-above
       bordered
       content-class="bg-grey-1"
     >
-      <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
-        </q-item-label>
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
+      <essential-link />
     </q-drawer>
-
     <q-page-container>
       <router-view />
     </q-page-container>
+    <q-ajax-bar
+      ref="bar"
+      position="bottom"
+      color="accent"
+      size="10px"
+      skip-hijack
+    />
   </q-layout>
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink.vue'
-
-const linksData = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
+import EssentialLink from '../components/EssentialLink.vue'
+import SelectLocalization from '../components/SelectLocalization.vue'
 
 export default {
   name: 'MainLayout',
-  components: { EssentialLink },
   data () {
     return {
-      leftDrawerOpen: false,
-      essentialLinks: linksData
+      leftDrawerOpen: false
     }
-  }
+  },
+  components: { EssentialLink, SelectLocalization }
 }
 </script>
