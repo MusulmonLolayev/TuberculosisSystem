1cc90de7339f288326f6ec6626be5ce3b1f69a3a
MusulmonLolayev <musulmon.lolayev.94@gmail.com>
2020-09-09 12:07:43 +0500

    The axios prototype was added store too.

--- frontend/gui-quasar/src/boot/axios.js
+++ frontend/gui-quasar/src/boot/axios.js
@@ -1,4 +1,13 @@
 import Vue from 'vue'
 import axios from 'axios'
 
-Vue.prototype.$axios = axios
+const axiosConfig = {
+    baseURL: 'http://127.0.0.1:8000/api',
+};
+
+let axiosIstance = axios.create(axiosConfig)
+
+export default ({store, Vue}) => {
+    Vue.prototype.$axios = axiosIstance,
+    store.$axios = axiosIstance
+}
