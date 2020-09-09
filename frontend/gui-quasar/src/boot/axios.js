0a02261f008456d5f2076fb9fdcf34d5ee7c8128
MusulmonLolayev <musulmon.lolayev.94@gmail.com>
2020-09-09 13:11:59 +0500

    The axiosInstance was refactored into Api due to funcions js.

--- frontend/gui-quasar/src/boot/axios.js
+++ frontend/gui-quasar/src/boot/axios.js
@@ -1,13 +1,12 @@
-import Vue from 'vue'
 import axios from 'axios'
 
 const axiosConfig = {
     baseURL: 'http://127.0.0.1:8000/api',
 };
 
-let axiosIstance = axios.create(axiosConfig)
+let Api = axios.create(axiosConfig)
 
 export default ({store, Vue}) => {
-    Vue.prototype.$axios = axiosIstance,
-    store.$axios = axiosIstance
+    Vue.prototype.$axios = Api,
+    store.$axios = Api
 }
