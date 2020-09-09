87b6d7d44f9e2159bbfdee774f5e29cafd43fb6a
MusulmonLolayev <musulmon.lolayev.94@gmail.com>
2020-09-09 15:22:28 +0500

    The headers was added into axios.

--- frontend/gui-quasar/src/boot/axios.js
+++ frontend/gui-quasar/src/boot/axios.js
@@ -2,6 +2,10 @@ import axios from 'axios'
 
 const axiosConfig = {
     baseURL: 'http://127.0.0.1:8000/api',
+    headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+    },
 };
 
 export const Api = axios.create(axiosConfig)
