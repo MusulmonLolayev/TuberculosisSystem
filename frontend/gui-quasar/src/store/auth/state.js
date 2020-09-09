06bd84768f9c594163e09a465e052cb48bedaa00
MusulmonLolayev <musulmon.lolayev.94@gmail.com>
2020-09-09 16:13:57 +0500

    The access_token logic was added into IsLoggined variable.

--- frontend/gui-quasar/src/store/auth/state.js
+++ frontend/gui-quasar/src/store/auth/state.js
@@ -1,6 +1,6 @@
 export default function () {
   return {
-    IsLoggined: false,
+    IsLoggined: localStorage.getItem('access_token') ? true : false,
     access_token: localStorage.getItem('access_token'),
     refresh_token: localStorage.getItem('refresh_token'),  
   }
