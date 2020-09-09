d9351e4d0d71083886e88df1ee900c5674d5655c
MusulmonLolayev <musulmon.lolayev.94@gmail.com>
2020-09-09 12:02:22 +0500

    The localization works.

---
+++ frontend/gui-quasar/src/components/SelectLocalization.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-select
+    v-model="lang"
+    :options="langOptions"
+    dense
+    borderless
+    emit-value
+    map-options
+    options-dense
+    style="min-width: 80px"
+  />
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      lang: this.$i18n.locale,
+      langOptions: [
+        { value: 'en-us', label: this.$t('lang_en') },
+        { value: 'uz', label: this.$t('lang_uz') }
+      ]
+    }
+  },
+  watch: {
+    lang (lang) {
+      localStorage.setItem('lang', lang)
+      this.$i18n.locale = lang
+    }
+  },
+  mounted: function () {
+    this.lang = localStorage.getItem('lang') || this.$i18n.locale
+  }
+}
+</script>
