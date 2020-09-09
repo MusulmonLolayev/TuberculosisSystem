e624c4bb2bf7ffc99e51110caeef8f6b4efc9a72
MusulmonLolayev <musulmon.lolayev.94@gmail.com>
2020-09-09 15:10:36 +0500

    The login methods in mutations and state were refactored.

--- frontend/gui-quasar/src/store/auth/mutations.js
+++ frontend/gui-quasar/src/store/auth/mutations.js
@@ -1,5 +1,7 @@
 export function login_success(state, {access_token, refresh_token}){
     state.IsLoggined = true
     localStorage.setItem('access_token', access_token)
+    state.access_token = access_token
+    state.refresh_token = refresh_token
     localStorage.setItem('refresh_token', refresh_token)
 }
--- frontend/gui-quasar/src/store/auth/state.js
+++ frontend/gui-quasar/src/store/auth/state.js
@@ -2,6 +2,6 @@ export default function () {
   return {
     IsLoggined: false,
     access_token: localStorage.getItem('access_token'),
-    refresh_token: localStorage.getItem('refresh_token'),    
+    refresh_token: localStorage.getItem('refresh_token'),  
   }
 }
