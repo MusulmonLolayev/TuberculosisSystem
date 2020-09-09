c6245c5d764397c43fc5cacca9f3c597fee91763
MusulmonLolayev <musulmon.lolayev.94@gmail.com>
2020-09-09 20:40:19 +0500

    The refresh_token method was added.

--- frontend/gui-quasar/src/store/auth/actions.js
+++ frontend/gui-quasar/src/store/auth/actions.js
@@ -4,3 +4,16 @@ export function login(context, {access_token, refresh_token}) {
         refresh_token: access_token
     })
 }
+
+export function refresh_token(context){
+    console.log(this.state.refresh_token)
+    this.$axios.post('/login/refresh', {
+        refresh: this.state.refresh_token
+    })
+    .then((response) => {
+        console.log(response.data)
+    })
+    .catch(() => {
+        this.$router.push('/login')
+    })
+}
--- frontend/gui-quasar/src/store/auth/mutations.js
+++ frontend/gui-quasar/src/store/auth/mutations.js
@@ -2,6 +2,11 @@ export function login_success(state, {access_token, refresh_token}){
     state.IsLoggined = true
     localStorage.setItem('access_token', access_token)
     state.access_token = access_token
-    state.refresh_token = refresh_token
     localStorage.setItem('refresh_token', refresh_token)
+    state.refresh_token = refresh_token
+}
+
+export function refresh_token(state, {access_token}){
+    localStorage.setItem('access_token', access_token)
+    state.access_token = access_token
 }
