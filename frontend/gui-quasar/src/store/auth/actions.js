ebb9d7f102b8bc2e4c212153d912dfc7c7fcc8c6
MusulmonLolayev <musulmon.lolayev.94@gmail.com>
2020-10-05 15:46:00 +0500

    The bug in refresh_token mutation was fixed.

--- frontend/gui-quasar/src/store/auth/actions.js
+++ frontend/gui-quasar/src/store/auth/actions.js
@@ -1,22 +1,11 @@
-export function login(context, {access_token, refresh_token}) {
+export function login(context, { access_token, refresh_token }) {
     context.commit('login_success', {
-        access_token: access_token, 
-        refresh_token: access_token
+        access_token: access_token,
+        refresh_token: refresh_token
     })
 }
 
-export function refresh_token(context){
-    delete this.$axios.defaults.headers.common["Authorization"]
-    console.log(this.$axios.defaults.headers.common)
-    this.$axios.post('/login/refresh', {
-        refresh: this.state.auth.refresh_token
-    })
-    .then((response) => {
-        console.log(response.data)
-        this.$axios.defaults.headers.common["Authorization"] = `Bearer ${response.data.access}`
-        context.commit('refresh_token', {refresh_token: response.data.access})
-    })
-    .catch(() => {
-        this.$router.push('/login')
-    })
+export function refresh_token(context, {access}) {
+    this.$axios.defaults.headers.common["Authorization"] = `Bearer ${access}`
+    context.commit('refresh_token', { access_token: access })
 }
