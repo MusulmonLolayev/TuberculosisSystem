064c7980697d613da46847641ff55373233ffbe6
MusulmonLolayev <musulmon.lolayev.94@gmail.com>
2020-10-09 22:45:04 +0500

    The error logics were added.

--- frontend/gui-quasar/src/boot/axios.js
+++ frontend/gui-quasar/src/boot/axios.js
@@ -1,5 +1,4 @@
 import axios from 'axios'
-import createAuthRefreshInterceptor from 'axios'
 
 const baseURL = 'http://127.0.0.1:8000/api'
 
@@ -21,7 +20,7 @@ function refreshToken(store) {
         {
           access: response.data.access
         })
-      store.dispatch('common/setIsRefreshingTokenExpired', {status: false})
+      store.dispatch('common/setIsRefreshingTokenExpired', { status: false })
       return Promise.resolve(true)
     })
     .catch(() => {
@@ -36,36 +35,54 @@ export default ({ store, Vue, router }) => {
   Api.defaults.headers.common['Authorization'] = `Bearer ${store.state.auth.access_token}`,
     Vue.prototype.$axios = Api,
     store.$axios = Api,
+    Api.interceptors.request.use(function (request) {
+      // Fisrt must to check the internet connection
+      if (!store.state.common.IsOnline) {
+        store.dispatch('common/no_internet_notify')
+        return Promise.reject(request)
+      }
+
+      return Promise.resolve(request)
+    }, error => {
+
+    }),
     Api.interceptors.response.use(response => response,
       function (error) {
         // Check error is Network error
-        if (!error.status){
+        if (!error.status) {
+          return Promise.reject(error)
+        }
+        // Check Internal server error
+        if (error.response.status === 500) {
+          store.dispatch('common/internal_server_error_dialog')
           return Promise.reject(error)
         }
         let data = error.response.data
+        // if there is the token is experid, then must the refresh the token
         if (!Is_Refreshed_Token &&
-          error.response.status === 401 && 
-          data && data.code && data.code == "token_not_valid" && 
+          error.response.status === 401 &&
+          data && data.code && data.code == "token_not_valid" &&
           store.state.auth.refresh_token) {
-            Is_Refreshed_Token = true
+          Is_Refreshed_Token = true
           return refreshToken(store).then(_ => {
             error.config.headers['Authorization'] = 'Bearer ' + store.state.auth.access_token;
             error.config.baseURL = undefined;
             return Api.request(error.config);
           })
-          .catch((error) => {
-            return Promise.reject(error)
-          })
+            .catch((error) => {
+              return Promise.reject(error)
+            })
         }
-        else{
+        else {
+          // If the refresh token is experid too, then the user must relogin 
           if (Is_Refreshed_Token) {
-            store.dispatch('common/setIsRefreshingTokenExpired', {status: true})
+            store.dispatch('common/setIsRefreshingTokenExpired', { status: true })
             router.push('/login')
           }
-          else{
+          else {
 
           }
-        } 
+        }
       }
     )
 }
